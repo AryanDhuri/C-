4140d44cfec09f5d6eda08811e9409627019b07c
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:53:07 +0530

    Add files via upload

---
+++ SealedClassMethod/SealedClassMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SealedClassMethod
+{
+    class Animal
+    {
+        public virtual void Makessound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        sealed public override void Makessound()
+        {
+            Console.WriteLine("Dog is Barking");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d=new Dog();
+            d.Makessound();
+        }
+    }
+}
+ 
