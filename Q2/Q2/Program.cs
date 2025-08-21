60c0a2199972bb3a1afc70454eb8413e2cf30d89
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:52:08 +0530

    Add files via upload

---
+++ Q2/Q2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Animal
+{
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping");
+    }
+}
+
+class Dog : Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Dog is eating");
+    }
+}
+
+class Program
+{
+   static void Main(string[] args)
+    {
+        Dog d=new Dog();
+        d.Sleep();
+        d.Eat();
+    }
+}
