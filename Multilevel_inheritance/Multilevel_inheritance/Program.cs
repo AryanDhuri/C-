cd0a142c23016c54e6f82662da73eaf0fe132c07
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:50:51 +0530

    Add files via upload

---
+++ Multilevel_inheritance/Multilevel_inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class University
+{
+    public void Usn()
+    {
+        Console.WriteLine("NU24CA010");
+    }
+}
+
+class College : University
+{
+    public void RegNo()
+    {
+        Console.WriteLine("010");
+    }
+}
+
+class Student : College
+{
+    public void RollNo()
+    {
+        Console.WriteLine("10");
+    }
+}
+ class Program()
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+        s.Usn();
+        s.RegNo();
+        s.RollNo();
+    }
+}
