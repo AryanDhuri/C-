c8b3e906a3d0bc78988bb307bcffdd77a41bcec9
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:50:19 +0530

    Add files via upload

---
+++ Encap/Encap/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Bank
+{
+    private int bank_account=010;
+    Console.WriteLine("Enter Bank account Number");
+    int value = Convert.ToInt32(Console.ReadLine());
+    public int Bank_account
+    {
+        get
+        {
+            return bank_account;
+        }
+        set
+        {
+            if (bank_account != value)
+            {
+                Console.WriteLine("Invalid");
+            }
+            else {
+                Console.WriteLine("Valid");
+            }
+
+        }
+    }
+
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bank b = new Bank();
+        b.Bank_account;
+    }
+}
