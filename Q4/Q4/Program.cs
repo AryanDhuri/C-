d4baae0744cf9f6756a760778668b402ba508bab
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:52:17 +0530

    Add files via upload

---
+++ Q4/Q4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class College
+{
+    protected void show_details()
+    {
+        Console.WriteLine("Nu24uca010");
+    }
+}
+
+class Student : College
+{
+    public void Display()
+    {
+        show_details();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s = new Student();
+        s.Display();
+    }
+}
