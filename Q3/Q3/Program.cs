fee456224ee730c0662f416b408337e72679bece
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:52:12 +0530

    Add files via upload

---
+++ Q3/Q3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lucky Ticket Numbers are:");
+        for(int i =1; i<=100; i++)
+        {
+            if(i%4==0 && i % 6 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
