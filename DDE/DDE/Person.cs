dc8c1773b2502dfafffb56b550d625305e1cbec0
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:49:39 +0530

    Add files via upload

---
+++ DDE/DDE/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace DataEncapsulationDemo
+{
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0 && value < 100)
+                    age = value;
+                else
+                    Console.WriteLine("Invalid age. Age must be between 0 and 100.");
+
+
+
+
+            }
+        }
+
+    }
+}
---
+++ DDE/DDE/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DDE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "Parker";
+            Console.WriteLine("Please Enter a Name:");
+            Person Age = 90;
+
+            Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+
+        }
+    }
+}
