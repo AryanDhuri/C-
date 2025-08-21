2a816aa2d35ea92b7b33b978780af90c44c16921
AryanDhuri <aryandhuri2899@gmail.com>
2025-08-21 10:51:36 +0530

    Add files via upload

---
+++ Q1/Q1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Bank
+{
+
+    private int bankaccount;
+    
+    public int Bank_account
+    {
+        get
+        {
+            return bank_account;
+        }
+        set
+        {
+            Console.WriteLine("Enter Bank account Number");
+            int n = Convert.ToInt32(Console.ReadLine(value));
+            if (bank_account != n)
+            {
+                Console.WriteLine("Invalid");
+            }
+            else
+            {
+                Console.WriteLine("Valid");
+            }
+
+        }
+    }
+
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bank b = new Bank();
+        b.Bank;
+    }
+}
