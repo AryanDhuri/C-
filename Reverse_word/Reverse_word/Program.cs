8baedc8787d472c280d31e04355495c99d337033
AryanDhuri <aryandhuri2899@gmail.com>
2025-09-04 10:47:46 +0530

    Add files via upload

---
+++ Reverse_word/Reverse_word/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class reverse_string
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("enter a sentence");
+        string str = Convert.ToString(Console.ReadLine());
+        string[] words = str.Split(' ').ToArray();
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            Console.Write(words[i]);
+        }
+    }
+
+}
