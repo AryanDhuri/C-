35c333a45f8fd4e8ff4530ccf18d59febd1fad2d
AryanDhuri <aryandhuri2899@gmail.com>
2025-09-18 10:04:34 +0530

    Add files via upload

---
+++ excep_hand/excep_hand/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter a Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int result = a / b;
+            Console.WriteLine($"Result: {result}");
+
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine(" Error: Division by zero");
+
+        }
+        catch (FormatException) 
+        {
+            Console.WriteLine(" Error: Format Exception");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed Succesfully");
+        }
+
+    }
+}
