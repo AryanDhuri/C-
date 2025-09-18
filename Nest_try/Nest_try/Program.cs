777de046f749a505772877e2a6dc6ea59fa89ab9
AryanDhuri <aryandhuri2899@gmail.com>
2025-09-18 10:34:11 +0530

    Add files via upload

---
+++ Nest_try/Nest_try/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        try
+        {
+            try
+            { 
+                Console.WriteLine("Enter a number");
+                int d = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a number");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                int dividebyzero = d / divisor;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Inner catch exception " + e.Message);
+            }
+        }
+        catch (DivideByZeroException e)
+        { 
+            Console.WriteLine("outer catch excecuted "+ e.Message);
+        
+        }
+    }
+}
